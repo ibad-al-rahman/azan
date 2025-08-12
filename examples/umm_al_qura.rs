366762a30b0249dec74cdda4943fd75b2b3941cc
Ghamza-Jd <hamzah.jadeed@gmail.com>
2025-08-12 13:02:55 +0300

    feat: added umm al qura example

---
+++ examples/umm_al_qura.rs
@@ -0,0 +1,56 @@
+use azan::prelude::*;
+
+fn main() {
+    println!("Prayer times for Makka in UTC");
+    println!("------------------------------");
+    println!();
+    let makka = Coordinates::new(21.427009, 39.828685);
+    let date = Utc::now().date_naive();
+    let params = Configuration::with(Method::UmmAlQura, Mazhab::Shafi);
+    let prayers = PrayerSchedule::new()
+        .on(date)
+        .for_location(makka)
+        .with_configuration(params)
+        .calculate();
+
+    match prayers {
+        Ok(prayer) => {
+            println!(
+                "{}: {}",
+                Prayer::Fajr.name(),
+                prayer.time(Prayer::Fajr).format("%-l:%M %p").to_string()
+            );
+            println!(
+                "{}: {}",
+                Prayer::Sunrise.name(),
+                prayer.time(Prayer::Sunrise).format("%-l:%M %p").to_string()
+            );
+            println!(
+                "{}: {}",
+                Prayer::Dhuhr.name(),
+                prayer.time(Prayer::Dhuhr).format("%-l:%M %p").to_string()
+            );
+            println!(
+                "{}: {}",
+                Prayer::Asr.name(),
+                prayer.time(Prayer::Asr).format("%-l:%M %p").to_string()
+            );
+            println!(
+                "{}: {}",
+                Prayer::Maghrib.name(),
+                prayer.time(Prayer::Maghrib).format("%-l:%M %p").to_string()
+            );
+            println!(
+                "{}: {}",
+                Prayer::Ishaa.name(),
+                prayer.time(Prayer::Ishaa).format("%-l:%M %p").to_string()
+            );
+            println!(
+                "{}: {}",
+                Prayer::Qiyam.name(),
+                prayer.time(Prayer::Qiyam).format("%-l:%M %p").to_string()
+            );
+        }
+        Err(error) => println!("Could not calculate prayer times: {}", error),
+    }
+}
