097f89e6cd45f39d333cb12e81a1adc564ac2292
Ghamza-Jd <hamzah.jadeed@gmail.com>
2026-01-04 23:24:44 +0200

    feat: added android codegen for azan

---
+++ android/azan/build.gradle.kts
@@ -0,0 +1,101 @@
+import com.android.build.gradle.internal.tasks.factory.dependsOn
+import org.gradle.configurationcache.extensions.capitalized
+
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.cargoNdk)
+    `maven-publish`
+}
+
+android {
+    namespace = GradleConfigs.subNamespace("azan")
+    compileSdk = GradleConfigs.compileSdk
+    ndkVersion = GradleConfigs.ndkVersion
+
+    defaultConfig {
+        minSdk = GradleConfigs.minSdk
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.jna)
+    implementation(libs.core.ktx)
+}
+
+cargoNdk {
+    module = ".."
+    librariesNames = arrayListOf("libazan.so")
+}
+
+afterEvaluate {
+    android.libraryVariants.all { variant ->
+        val outDir = "${layout.buildDirectory}/generated/source/uniffi/${variant.name}/java"
+
+        val generateBindings = tasks.register(
+            name = "generate${variant.name.capitalized()}UniFFIBindings",
+            type = Exec::class
+        ) {
+            workingDir = file("../..")
+            commandLine(
+                "cargo", "run", "-p", "uniffi-bindgen", "generate",
+                "--library", "./android/azan/src/main/jniLibs/arm64-v8a/libazan.so",
+                "--language", "kotlin",
+                "--out-dir", outDir
+            )
+            dependsOn("buildCargoNdk${variant.name.capitalized()}")
+        }
+
+        val copyBindings = tasks.register(
+            name = "copy${variant.name.capitalized()}UniFFIBindings",
+            type = Exec::class
+        ) {
+            workingDir = file("../..")
+            commandLine("cp", "-r", outDir, "${projectDir}/src/main/")
+            dependsOn(generateBindings)
+        }
+        variant.javaCompileProvider.dependsOn(copyBindings)
+        tasks.named("compile${variant.name.capitalized()}Kotlin") { dependsOn(generateBindings) }
+        tasks.named("connectedDebugAndroidTest").configure { dependsOn(generateBindings) }
+        true
+    }
+}
+
+configure<PublishingExtension> {
+    publications {
+        create<MavenPublication>("${project.name}-release") {
+            artifactId = "azan"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
+}
